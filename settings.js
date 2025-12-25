74fe3fb4e650c2ffb3bc082e3db77ad15f508cdb
muhammadabubaker1580-crypto <muhammadabubaker1580@gmail.com>
2025-12-25 14:02:12 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'ABUBAKER MD',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "ABUBAKER MD",
+  botOwner: 'ABUBAKER', // Your name
+  ownerNumber: '989120924073', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.5",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/mruniquehacker/ABUBAKER-MD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
